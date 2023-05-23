b4e376abec42802771d15b36697bc5c258f96abc
danyamdev <danya.mamin.99@bk.ru>
2023-05-23 20:17:20 +0300

    AMD-1: updated app

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'consistent',
+  arrowParens: 'avoid',
+  jsxBracketSameLine: false,
+  endOfLine: 'auto',
+  printWidth: 120
+};

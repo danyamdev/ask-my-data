1631bc8438ba1406cf6d14b57f5bce6ca2d25414
danyamdev <danya.mamin.99@bk.ru>
2023-05-30 10:41:03 +0300

    AMD-6: updateTableProperties

--- src/api/data/index.ts
+++ src/api/data/index.ts
@@ -6,11 +6,16 @@ import { TFile, TTablePage } from '../types/data';
 
 const dataAPI = {
   postUploadNewTable: () => bffAxios.post<TFile>('/uploadNewTable'),
+
   getTableAnalysis: () => bffAxios.get<TFile>(`/getTableAnalysis?token=${Cookies.get('token')}`).then(res => res.data),
+
   getTablePage: ({ page, rowsPerPage }: { page: number; rowsPerPage: number }) =>
     bffAxios
       .get<TTablePage>(`/getTablePage?token=${Cookies.get('token')}&page=${page}&rows_per_page=${rowsPerPage}`)
       .then(res => res.data),
+
+  updateTableProperties: (properties: string) =>
+    bffAxios.put(`/updateTableProperties?token=${Cookies.get('token')}&=${properties}`).then(res => res.data),
 };
 
 export default dataAPI;
